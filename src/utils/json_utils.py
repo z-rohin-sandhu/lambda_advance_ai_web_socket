297e3414746deaba92cc536e78dce70c5e596ac6
z-rohin-sandhu <rohin.sandhu@zenarate.com>
2025-12-19 17:13:36 +0530

    json changes

--- src/utils/json_utils.py
+++ src/utils/json_utils.py
@@ -1,7 +1,16 @@
 import json
+from src.utils.logging import log
 
 def json_dumps(data):
-    return json.dumps(data)
+    try:
+        return json.dumps(data)
+    except Exception as exc:
+        log("json_dumps failed", level="ERROR", error=str(exc))
+        return {}
 
 def json_loads(data):
-    return json.loads(data)
+    try:
+        return json.loads(data)
+    except Exception as exc:
+        log("json_loads failed", level="ERROR", error=str(exc))
+        return {}
--- src/websocket/sender.py
+++ src/websocket/sender.py
@@ -1,39 +1,39 @@
-import json
 import boto3
 from src.websocket.response import build_ws_response
 from src.utils.logging import log
-
-
+from src.utils.json_utils import json_dumps
 
 class WebSocketSender:
     def __init__(self, event: dict):
-        self.connection_id = event["requestContext"]["connectionId"]
-        domain_name = event["requestContext"]["domainName"]
-        stage = event["requestContext"]["stage"]
-
-        endpoint_url = f"https://{domain_name}/{stage}"
+        self.connection_id = event.get("requestContext", {}).get("connectionId")
+        self.domain_name = event.get("requestContext", {}).get("domainName")
+        self.stage = event.get("requestContext", {}).get("stage")
+        self.endpoint_url = f"https://{self.domain_name}/{self.stage}"
 
         log(
-            "[WebSocketSender] init "
+            "WebSocketSender initialized",
             f"connection_id={self.connection_id}, "
-            f"endpoint={endpoint_url}"
+            f"domain_name={self.domain_name}, "
+            f"stage={self.stage}, "
+            f"endpoint_url={self.endpoint_url}"
         )
 
         self.client = boto3.client(
             "apigatewaymanagementapi",
-            endpoint_url=endpoint_url,
+            endpoint_url=self.endpoint_url,
         )
 
     def send(self, action: str, data: dict | None = None) -> None:
-        payload = build_ws_response(action=action, data=data)
+        response = build_ws_response(action=action, data=data)
 
         log(
             "WebSocketSender.send",
             connection_id=self.connection_id,
             action=action,
+            response=response
         )
 
         self.client.post_to_connection(
             ConnectionId=self.connection_id,
-            Data=json.dumps(payload).encode("utf-8"),
+            Data=json_dumps(response).encode("utf-8"),
         )
